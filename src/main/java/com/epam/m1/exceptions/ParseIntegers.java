caa51cdaeefab0fad5f4a05d6249d9f7ca941b24
Utkirbek Qutfiddinov <utkirbek_qutfiddinov@mail.ru>
2023-11-11 00:04:49 +0500

    done

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -18,11 +18,15 @@ public class ParseIntegers {
     public static void main(String[] args) {
         Iterator<String> words = WORDS.iterator();
         int sum = 0;
-        String justWords = "";
+        StringBuilder justWords = new StringBuilder();
         while (words.hasNext()) {
             String next = words.next();
-            int number = Integer.parseInt(next);
-            // todo: complete it
+            try{
+                int number = Integer.parseInt(next);
+                sum+=number;
+            }catch (NumberFormatException e){
+                justWords.append(" ").append(next);
+            }
         }
         System.out.println("Sum is " + sum);
         System.out.println("Just words:" + justWords);
